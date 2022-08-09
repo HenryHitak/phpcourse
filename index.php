a8c8f74b0c0ebcadf307a7e11c454a93103cdd7e
HenryHitak <gmlxkr920901@gmail.com>
2022-08-08 18:39:58 -0700

    php init

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- echo:print this line always start by using ?php and end with ?  and each line when it is done with semicolon.
+        $name-->
+    <?php
+     $name = "Hitak";
+     $country = "Korea";
+     $hobby = "hobbyyyyyyyyy";
+     $name1 = "Milad";
+     echo "Hello World","<h1>Hitak Lee</h1>","<h4>Korea</h4>","<h5>Hobby</h5>";
+     echo "Hello World <h1>Hitak Lee</h1><h4>Korea</h4><h5>Hobby</h5>";
+     echo "<h1>Hitak Lee</h1>";
+     echo "<h4>My name is :$name</h4><h4>I am from: $country</h4><h4>There is no $hobby</h4>";
+     echo "<h5>Hobby</h5>";
+    ?>
+</body>
+</html>
